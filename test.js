1c00d80ebe372b7ed56d87c433ba9fc8c4b4d372
Chiptune <modchiptune@google.com>
2019-01-24 14:27:16 +0900

    .

---
+++ test.js
@@ -0,0 +1,15 @@
+const
+	fs=require('fs'),
+	sync=require('./sync.json');
+
+fs.readdir('singlefile1',(err,files)=>{
+	if(err)return;
+	for(var i=files.length-1;i>=0;i--){
+		const team=files[i].split(' - ')[0];
+		//fs.mkdir('data/'+team,(err)=>{});
+		if(files[i] in sync)continue;
+		sync[files[i]]={team};
+		fs.copyFile('singlefile1/'+files[i],'data/'+team+'/'+files[i],(err)=>{});
+	}
+	fs.writeFile('sync.json',JSON.stringify(sync),(err)=>{});
+});
