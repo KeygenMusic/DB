0e2856caa7bcdfbc552a1a6a2b961e455b9b8873
Chiptune <modchiptune@google.com>
2019-01-24 14:29:08 +0900

    .

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ fs.readdir('singlefile1',(err,files)=>{
 	for(var i=files.length-1;i>=0;i--){
 		const team=files[i].split(' - ')[0];
 		//fs.mkdir('data/'+team,(err)=>{});
-		if(files[i] in sync)continue;
+		if(files[i] in sync||/(\.(ogg|m4a)^)/.exec(files[i]))continue;
 		sync[files[i]]={team};
 		fs.copyFile('singlefile1/'+files[i],'data/'+team+'/'+files[i],(err)=>{});
 	}
